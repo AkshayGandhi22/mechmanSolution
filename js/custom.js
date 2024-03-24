f9d2d9c52e8cc535626b8e03687583514e2974d5
Akshay Gandhi <akshay@zurumedia.com>
2024-03-24 14:14:58 +0530

    solution and software pages are done

--- js/custom.js
+++ js/custom.js
@@ -103,14 +103,30 @@ createAccordian(document.querySelector('app-accordion[id="autoDeskAutocad"]'), [
   var currentPage = window.location.pathname;
   var pathSprit = currentPage.split("/")
   var lastElement = pathSprit.slice(-2, -1)[0]
+ 
 
   // Select navigation items
   var navigationItems = document.querySelectorAll('#navbarSupportedContent a');
+  var mainNavigationItems = document.querySelectorAll('#navbarSupportedContent a.nav-link');
 
   // Remove "active" class from all navigation items
   navigationItems.forEach(function(item) {
     item.classList.remove('active');
   });
+  mainNavigationItems.forEach(function(item) {
+    item.classList.remove('active');
+  });
+
+  for (var i = 0; i < mainNavigationItems.length; i++) {
+    let innerText = mainNavigationItems[i].innerText
+    let covertedText = innerText.replace(/\s+/g, '').toLowerCase();
+    pathSprit.find((currentValue) => {
+      if (currentValue === covertedText) {
+        mainNavigationItems[i].classList.add('active');
+        return true; // stop the iteration once a match is found
+      }
+    });
+  }
 
   // Add "active" class to the navigation item corresponding to the current page
   for (var i = 0; i < navigationItems.length; i++) {
